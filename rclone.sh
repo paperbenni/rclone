7741be5f2ef392d145fa0356658b728a86745fb2
paperbenni <paperbenni@gmail.com>
2018-07-29 13:17:51 +0200

    set up github pages with colors

--- rclone.sh
+++
@@ -1,6 +0,0 @@
-RCLONEDIR=$(pwd)/rclone
-mkdir rclone
-cd rclone
-curl -o rclone https://github.com/paperbenni/rclone/raw/master/rclone
-curl https://raw.githubusercontent.com/paperbenni/rclone/master/rclone.1 > rclone.1
-export PATH="$RCLONEDIR:${PATH}"
